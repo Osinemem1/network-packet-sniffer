ffa1596ad69a6c712fc28d49af56a65ed69a187d
Osinemem1 <harrisonnwoti@gmail.com>
2025-09-09 09:31:45 +0100

    feat: successfully parsing IPv4 headers - sniffer fully operational!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use pcap::{Capture, Device};
 use std::fmt::Write;
-
+use std::net::Ipv4Addr;
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     println!("📡 NetSniff - Packet Sniffer");
     println!("============================");
@@ -19,8 +19,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     .open()?;
 
 println!("\nStarting capture. Press Ctrl+C to stop.\n");
-println!("{:<18} {:<18} {:<18} {:<6}", "Source MAC", "Destination MAC", "Protocol", "Length");
-println!("{:-<60}", "");
+println!("{:<18} {:<18} {:<15} {:<15} {:<6}", "Source MAC", "Destination MAC", "Source IP", "Destination IP", "Length");
+println!("{:-<80}", "");
 
 
 while let Ok(packet) = cap.next_packet() {
@@ -29,13 +29,28 @@ while let Ok(packet) = cap.next_packet() {
     if packet.data.len() >= 14 {
 
         let (src_mac, dst_mac, ether_type) = parse_ethernet_frame(&packet.data);
-        println!("{:<18} {:<18} {:<18} {:<6}", src_mac, dst_mac, format!("0x{0:04X}", ether_type), length);
- } else {
-        println!("{:<18} {:<18} {:<8} {:<6}", "Malformed", "Packet", "N/A", length);
+
+
+    if ether_type == 0x0800 && packet.data.len() >= 34 {
+
+        if let Some((src_ip, dst_ip)) = parse_ipv4_header(&packet.data[14..]){
+            println!("{:<18} {:<18} {:<15} {:<15}  {:<6}", src_mac, dst_mac, src_ip, dst_ip, length);
+        } else {
+        println!("{:<18} {:<18} {:<15} {:<15} {:<6}", src_mac, dst_mac ,"Malformed", "IP", length);
+ }
+
+}else{
+     println!("{:<18} {:<18} {:<15} {:<15} {:<6}", src_mac, dst_mac ,"Non-IPV4", "Packet", length);
+ }
+}else{
+     println!("{:<18} {:<18} {:<15} {:<15} {:<6}", "Malformed", "Packet", "N/A", "N/A", length);
  }
 }
 Ok(())
-}
+ } 
+
+
+
 fn parse_ethernet_frame(data: &[u8]) -> (String, String, u16) {
     let dst_mac = format_mac_address(&data[0..6]);
 
@@ -44,10 +59,21 @@ fn parse_ethernet_frame(data: &[u8]) -> (String, String, u16) {
     let ether_type = u16::from_be_bytes([data[12], data[13]]);
     (src_mac, dst_mac, ether_type)
 }
+fn parse_ipv4_header(data: &[u8]) -> Option<(String, String)> {
+    if data.len() < 20 {
+        return None;
+    }
 
-fn format_mac_address(byts: &[u8]) -> String {
+    let src_ip = Ipv4Addr::new(data[12], data[13], data[14], data[15]);
+
+     let dst_ip = Ipv4Addr::new(data[16], data[17], data[18], data[19]);
+
+     Some((src_ip.to_string(), dst_ip.to_string()))
+}
+
+fn format_mac_address(bytes: &[u8]) -> String {
     let mut mac = String::new();
-    for (i, byte) in byts.iter().enumerate() {
+    for (i, byte) in bytes.iter().enumerate() {
         if i > 0 {
             write!(&mut mac, ":").unwrap();
         }
